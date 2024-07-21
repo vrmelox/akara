75f838eb9b2dee2c3a327ba7fb8d3b04492e44df
Philippe Akandé <akandeabiodoun@gmail.com>
2024-07-21 18:51:07 +0100

    Create index.php
    I've created index.php and inserted into it the header and footer pages.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Akara</title>
+</head>
+<body>
+<?php require_once(__DIR__ . '/header.php'); ?>
+<?php require_once(__DIR__ . '/footer.php'); ?>
+</body>
+</html>
