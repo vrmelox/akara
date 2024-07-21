1800747620a063c3b68eb6f3ad9b948ff56ad8b9
Philippe Akandé <akandeabiodoun@gmail.com>
2024-07-21 18:28:25 +0100

    Create header.php
    This is my header's file. i will use it on every page of Akara website.

---
+++ header.php
@@ -0,0 +1,8 @@
+<nav class="menu">
+    <ul>
+        <li><a href="akara.bj/accueil">Accueil</a></li>
+        <li><a href="akara.bj/menu">Menu</a></li>
+        <li><a href="akara.bj/services">Services</a></li>
+        <li><a href="akara.bj/contact">Contact</a></li>
+    </ul>
+</nav>
