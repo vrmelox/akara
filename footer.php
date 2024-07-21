9bc0349a0822651ce247838c12c692c76978b298
Philippe Akandé <akandeabiodoun@gmail.com>
2024-07-21 18:45:14 +0100

    Create footer.php
    I've created the footer page in on file, so it can the insert into every page of the website.

---
+++ footer.php
@@ -0,0 +1,31 @@
+<foot>
+    <div class="presentation">
+        <div class="logo">
+
+        </div>
+        <div class="brevePresentation">
+            <p>Akara est une florissante entreprise de restauration spécialisée
+                dans la réalisation des beignets et autres brioches. Depuis 55 ans,
+                nos beignets de haricot sont appréciés.
+            </p>
+        </div>
+        <div class="footMenu">
+            <h3>Explorer</h3>
+            <ul>
+                <li><a href="akara.bj/beignets">Beignets</a></li>
+                <li><a href="akara.bj/learn">Apprendre</a></li>
+                <li><a href="akara.bj/towe">Towé</a></li>
+                <li><a href="akara.bj/cheffe">Cheffe</a></li>
+            </ul>
+        </div>
+        <div class="contacts">
+            <h3>Pour nous joindre</h3>
+            <p>Pobè, Towé, Rue 1, 074 ABIODOUN</p>
+            <p><a href="mailto:lydiaok@akara.bj">lydiaok@akara.bj</a></p>
+            <p><a href="tel:+22995662252">+229 95662252</a></p>
+        </div>
+    </div>
+    <div class="copyright">
+        <p>&copy; <?php echo date("Y"); ?> | Akara. Tous droits réservés</p>
+    </div>
+</foot>
